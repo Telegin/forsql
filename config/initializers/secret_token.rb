c47daddc156c543bc1d537d20826a07125e280d2
Malafy <grigorymalafy@gmail.com>
2013-01-12 03:48:12 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forsql::Application.config.secret_token = 'a54b657ff53640bb1a50942b1d0379391b0dbec60c76a3e320d713fa7b912050715e0fafbdce82fc073a2b8b382903782ed4a762b6cc2e8ce8a36f82e4ee6f36'
